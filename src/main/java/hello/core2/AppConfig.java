8b490bd4c124f43389ab8498a0ab1b079602e699
junsoo <choix712@umn.edu>
2023-08-01 21:46:37 +0900

    Singleton Container

--- src/main/java/hello/core2/AppConfig.java
+++ src/main/java/hello/core2/AppConfig.java
@@ -12,21 +12,37 @@ import hello.core2.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+// @Configuration
 public class AppConfig {
 
+    // @Bean -> memberSErvice -> new MemoryMemberRepository()
+    // @Bean orderService -> new MemoryMemberRepository()
+
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+    // call AppConfig.memberRepository
+
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+    // 세번이 호출되어야 하는데 한번만 출력되었다.
     @Bean
     public MemberService memberService (){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
--- src/test/java/hello/core2/singleton/SingletonTest.java
+++ src/test/java/hello/core2/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import hello.core2.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -27,5 +29,35 @@ public class SingletonTest {
 
         // memberService1 != 2
         assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        Singletonservice singletonService1 = Singletonservice.getInstance();
+        Singletonservice singletonService2 = Singletonservice.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        // AppConfig appConfig = new AppConfig();
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조값이 다른것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != 2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
